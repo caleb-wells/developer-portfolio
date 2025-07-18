7a88ca04d2289621f6be7dfb6a9171c29711cd43
Caleb Wells <wcaleb650@gmail.com>
2025-07-17 22:28:04 -0600

    add deploy action, update readme, add required cdk code for AWS hosting of Astro site

--- infrastructure/cdk/bin/infrastructure.ts
+++ infrastructure/cdk/bin/infrastructure.ts
@@ -3,18 +3,20 @@ import * as cdk from 'aws-cdk-lib';
 import { InfrastructureStack } from '../lib/infrastructure-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const domainName = app.node.tryGetContext('domainName');
+const hostedZoneId = app.node.tryGetContext('hostedZoneId');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+if (!domainName) {
+  throw new Error('domainName context variable is required. Use: cdk deploy -c domainName=example.com');
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new InfrastructureStack(app, 'PortfolioStack', {
+  domainName,
+  hostedZoneId,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-1', // ACM certificates for CloudFront must be in us-east-1
+  },
+  crossRegionReferences: true,
 });
--- infrastructure/cdk/lib/infrastructure-stack.ts
+++ infrastructure/cdk/lib/infrastructure-stack.ts
@@ -1,16 +1,237 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export interface InfrastructureStackProps extends cdk.StackProps {
+  domainName: string;
+  hostedZoneId?: string;
+}
 
 export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: InfrastructureStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { domainName, hostedZoneId } = props;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // S3 bucket for static website hosting
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      bucketName: `${domainName}-website`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: true,
+      lifecycleRules: [
+        {
+          id: 'DeleteOldVersions',
+          enabled: true,
+          noncurrentVersionExpiration: cdk.Duration.days(30),
+        },
+      ],
+      serverAccessLogsPrefix: 'access-logs/',
+    });
+
+    // Origin Access Control for CloudFront
+    const originAccessControl = new cloudfront.S3OriginAccessControl(this, 'OriginAccessControl', {
+      description: `OAC for ${domainName}`,
+    });
+
+    // Route53 hosted zone (reference existing or create new)
+    const hostedZone = hostedZoneId
+      ? route53.HostedZone.fromHostedZoneId(this, 'HostedZone', hostedZoneId)
+      : new route53.HostedZone(this, 'HostedZone', {
+          zoneName: domainName,
+        });
+
+    // SSL Certificate
+    const certificate = new acm.Certificate(this, 'Certificate', {
+      domainName,
+      subjectAlternativeNames: [`www.${domainName}`],
+      validation: acm.CertificateValidation.fromDns(hostedZone),
+    });
+
+    // CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket, {
+          originAccessControl,
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        compress: true,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        responseHeadersPolicy: this.createSecurityHeadersPolicy(),
+      },
+      domainNames: [domainName, `www.${domainName}`],
+      certificate,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 404,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(5),
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 404,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.minutes(5),
+        },
+      ],
+      enableLogging: true,
+      logBucket: websiteBucket,
+      logFilePrefix: 'cloudfront-logs/',
+      logIncludesCookies: false,
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      comment: `CloudFront distribution for ${domainName}`,
+    });
+
+    // Grant CloudFront access to S3 bucket
+    websiteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        sid: 'AllowCloudFrontServicePrincipal',
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+        actions: ['s3:GetObject'],
+        resources: [websiteBucket.arnForObjects('*')],
+        conditions: {
+          StringEquals: {
+            'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+          },
+        },
+      })
+    );
+
+    // Route53 A record for apex domain
+    new route53.ARecord(this, 'ARecord', {
+      zone: hostedZone,
+      recordName: domainName,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+
+    // Route53 A record for www subdomain
+    new route53.ARecord(this, 'WwwARecord', {
+      zone: hostedZone,
+      recordName: `www.${domainName}`,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+
+    // IAM user for GitHub Actions deployment
+    const deploymentUser = new iam.User(this, 'DeploymentUser', {
+      userName: `${domainName}-deployment-user`,
+    });
+
+    // Policy for deployment user
+    const deploymentPolicy = new iam.Policy(this, 'DeploymentPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: [
+            's3:PutObject',
+            's3:PutObjectAcl',
+            's3:GetObject',
+            's3:DeleteObject',
+            's3:ListBucket',
+          ],
+          resources: [websiteBucket.bucketArn, websiteBucket.arnForObjects('*')],
+        }),
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ['cloudfront:CreateInvalidation'],
+          resources: [distribution.distributionArn],
+        }),
+      ],
+    });
+
+    deploymentUser.attachInlinePolicy(deploymentPolicy);
+
+    // Outputs
+    new cdk.CfnOutput(this, 'WebsiteBucketName', {
+      value: websiteBucket.bucketName,
+      description: 'Name of the S3 bucket for website content',
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontDistributionId', {
+      value: distribution.distributionId,
+      description: 'CloudFront Distribution ID',
+    });
+
+    new cdk.CfnOutput(this, 'CloudFrontDistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'CloudFront Distribution Domain Name',
+    });
+
+    new cdk.CfnOutput(this, 'DeploymentUserName', {
+      value: deploymentUser.userName,
+      description: 'Name of the deployment user for GitHub Actions',
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteUrl', {
+      value: `https://${domainName}`,
+      description: 'Website URL',
+    });
+
+    if (!hostedZoneId) {
+      new cdk.CfnOutput(this, 'NameServers', {
+        value: hostedZone.hostedZoneNameServers!.join(', '),
+        description: 'Name servers for the hosted zone (configure these in domain registrar)',
+      });
+    }
+  }
+
+  private createSecurityHeadersPolicy(): cloudfront.ResponseHeadersPolicy {
+    return new cloudfront.ResponseHeadersPolicy(this, 'SecurityHeadersPolicy', {
+      comment: 'Security headers for static website',
+      securityHeadersBehavior: {
+        strictTransportSecurity: {
+          accessControlMaxAge: cdk.Duration.days(365),
+          includeSubdomains: true,
+          preload: true,
+          override: true,
+        },
+        contentTypeOptions: {
+          override: true,
+        },
+        frameOptions: {
+          frameOption: cloudfront.HeadersFrameOption.DENY,
+          override: true,
+        },
+        referrerPolicy: {
+          referrerPolicy: cloudfront.HeadersReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN,
+          override: true,
+        },
+      },
+      customHeadersBehavior: {
+        customHeaders: [
+          {
+            header: 'X-Content-Type-Options',
+            value: 'nosniff',
+            override: true,
+          },
+          {
+            header: 'X-Frame-Options',
+            value: 'DENY',
+            override: true,
+          },
+          {
+            header: 'X-XSS-Protection',
+            value: '1; mode=block',
+            override: true,
+          },
+          {
+            header: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=()',
+            override: true,
+          },
+        ],
+      },
+    });
   }
 }
